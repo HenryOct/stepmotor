f00c19f36ceee6b59f25b20d97c0a9b82a85753a
HenryOct <henrywan0916@gmail.com>
2026-08-13 17:23:35 +0800

    cursor conversation cloud stored

--- UserApp/led_task.cpp
+++ UserApp/led_task.cpp
@@ -2,9 +2,11 @@
 #include "main.h"
 #include "stm32f1xx_hal.h"
 
-extern "C" void led_task() {
-  while (1) {
+extern "C" void led_task()
+{
+  while (1)
+  {
     HAL_GPIO_TogglePin(led_GPIO_Port, led_Pin);
-    HAL_Delay(1000);
+    HAL_Delay(100);
   }
 }
